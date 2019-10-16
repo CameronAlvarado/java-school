3bc7fc39c4b9fff956a6f59cc546453e84ca50cb
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2019-10-16 13:26:32 -0700

    working on swagger endpoints

--- school/src/main/java/com/lambdaschool/school/controller/StudentController.java
+++ school/src/main/java/com/lambdaschool/school/controller/StudentController.java
@@ -1,10 +1,14 @@
 package com.lambdaschool.school.controller;
 
+import com.lambdaschool.school.model.ErrorDetail;
 import com.lambdaschool.school.model.Student;
 import com.lambdaschool.school.service.StudentService;
+import io.swagger.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,7 +28,34 @@ public class StudentController
     @Autowired
     private StudentService studentService;
 
+    @ApiOperation(value = "return all Students",
+            response = Student.class,
+            responseContainer = "List")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "page", dataType = "integer", paramType = "query",
+                    value = "Results page you want to retrieve (0..N)"),
+            @ApiImplicitParam(name = "size", dataType = "integer", paramType = "query",
+                    value = "Number of records per page."),
+            @ApiImplicitParam(name = "sort", allowMultiple = true, dataType = "string", paramType = "query",
+                    value = "Sorting criteria in the format: property(,asc|desc). " +
+                            "Default sort order is ascending. " +
+                            "Multiple sort criteria are supported.")})
 
+    // http://localhost:2019/students/students/paging/
+    // http://localhost:2019/students/students/paging/?page=1
+    // http://localhost:2019/students/students/paging/?page=0&size=10
+    // http://localhost:2019/students/students/paging/?page=0&size=10&sort=studname
+    // http://localhost:2019/students/students/paging/?page=0&size=10&sort=studname,desc
+    @GetMapping(value = "/students/paging",
+            produces = {"application/json"})
+    public ResponseEntity<?> listAllStudentsByPage(@PageableDefault(page = 0, size = 5) Pageable pageable)
+    {
+        List<Student> myStudents = studentService.findAllPageable(pageable);
+//      List<Student> myStudents = studentService.findAllPageable(Pageable.unpaged()); <-- un-pages using code.
+        return new ResponseEntity<>(myStudents, HttpStatus.OK);
+    }
+
+    
     private static final Logger logger = LoggerFactory.getLogger(StudentController.class);
     //               slf4j ^
 
@@ -35,15 +66,20 @@ public class StudentController
     {
         logger.warn("This is a warn log");
         logger.trace("This is a trace log");
-
         logger.info(request.getMethod() + " " + request.getRequestURI() + " accessed");
+
         List<Student> myStudents = studentService.findAll();
         return new ResponseEntity<>(myStudents, HttpStatus.OK);
     }
 
-    @GetMapping(value = "/Student/{StudentId}",
-                produces = {"application/json"})
+    @ApiOperation(value = "Retrieves a student accociated with the student ID", response = Student.class)
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Student Found", response = Student.class),
+    @ApiResponse(code = 404, message = "Student Not Found", response = ErrorDetail.class)})
+
+    @GetMapping(value = "/student/{StudentId}",
+            produces = {"application/json"})
     public ResponseEntity<?> getStudentById(
+            @ApiParam(value = "Student ID", required = true, example = "3")
             @PathVariable
                     Long StudentId)
     {
@@ -53,7 +89,7 @@ public class StudentController
 
 
     @GetMapping(value = "/student/namelike/{name}",
-                produces = {"application/json"})
+            produces = {"application/json"})
     public ResponseEntity<?> getStudentByNameContaining(
             @PathVariable String name)
     {
@@ -63,8 +99,8 @@ public class StudentController
 
 
     @PostMapping(value = "/Student",
-                 consumes = {"application/json"},
-                 produces = {"application/json"})
+            consumes = {"application/json"},
+            produces = {"application/json"})
     public ResponseEntity<?> addNewStudent(@Valid
                                            @RequestBody
                                                    Student newStudent) throws URISyntaxException
